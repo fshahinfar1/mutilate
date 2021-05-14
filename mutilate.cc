b86486fdc7c43c4fd74e8afddb235b5fb6790a40
Farbod Shahinfar <fshahinfar1@gmail.com>
2021-05-14 10:44:09 -0600

    turn off anoying (debug) message about agent sampling

--- mutilate.cc
+++ mutilate.cc
@@ -724,9 +724,9 @@ void do_mutilate(const vector<string>& servers, options_t& options,
     bool sampling = true;
     if (args.agentmode_given && !options.should_sample)
       sampling = false;
-    if (sampling) {
-      printf("Agent sampling is enabled!\n");
-    }
+    // if (sampling) {
+    //   printf("Agent sampling is enabled!\n");
+    // }
     for (int c = 0; c < conns; c++) {
       int src_port = args.src_port_given ? src_ports[c] : 0;
       TCPConnection* conn = new TCPConnection(base, evdns, hostname, port, options,
